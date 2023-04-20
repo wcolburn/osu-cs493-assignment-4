a68b07f9f845fb934efba4274d447a3ee7410318
Brian "Beej Jorgensen" Hall <beej@beej.us>
2023-04-20 15:35:04 -0700

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const { Router } = require('express')
+
+const router = Router()
+
+router.use('/businesses', require('./businesses'))
+router.use('/photos', require('./photos'))
+
+module.exports = router
